7b12893308ac0735b989c10f9628b718cb893da1
TecHaxter <mr.khushalrao@gmail.com>
2022-01-31 01:12:20 +0530

    latest by khushal --- generated key needs to be shown

--- app.py
+++ app.py
@@ -1,35 +1,49 @@
-from flask import *  
-app = Flask(__name__)  
-import io
 from cryptography.fernet import Fernet
-import base64
+import io
+from flask import *
 
-@app.route('/')  
-def upload():  
-    return render_template("upload.html")  
- 
-@app.route('/decrypt', methods = ['POST'])  
-def success():  
-    if request.method == 'POST':  
+app = Flask(__name__)
+
+
+@app.route('/')
+def upload():
+    return render_template("upload.html")
+
+
+@app.route('/decrypt', methods=['POST'])
+def decryptRequest():
+    if request.method == 'POST':
         encrypted_file = request.files['file']
         key = request.form['key']
         decrypted_data = decrypt(encrypted_data=encrypted_file.read(), key=key)
         return send_file(io.BytesIO(decrypted_data), as_attachment=True,
-                     attachment_filename=encrypted_file.filename,
-                     mimetype=encrypted_file.mimetype)
+                         attachment_filename=encrypted_file.filename,
+                         mimetype=encrypted_file.mimetype)
 
-def encrypt(filename, key):
+
+@app.route('/encrypt', methods=['POST'])
+def encryptRequest():
+    if request.method == 'POST':
+        original_file = request.files['file']
+        encrypted_data, encrypt_key = encrypt(
+            original_data=original_file.read())
+        return send_file(io.BytesIO(encrypted_data), as_attachment=True,
+                         attachment_filename=original_file.filename,
+                         mimetype=original_file.mimetype)
+
+
+def encrypt(original_data):
+    key = Fernet.generate_key()
     f = Fernet(key)
-    with open(filename, "rb") as file:
-        file_data = file.read()
-    encrypted_data = f.encrypt(file_data)
-    with open(filename, "wb") as file:
-        file.write(encrypted_data)
+    encrypted_data = f.encrypt(original_data)
+    return encrypted_data, key
+
 
 def decrypt(encrypted_data, key):
     f = Fernet(key)
     decrypted_data = f.decrypt(encrypted_data)
     return decrypted_data
 
-if __name__ == '__main__':  
-    app.run(debug = True)  
+
+if __name__ == '__main__':
+    app.run(debug=True)
