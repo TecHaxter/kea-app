4ecd8ed21d0bf2b68c6b30a9394ef73a04924262
Arindam Ghosh <arindamghosh7878@gmail.com>
2022-02-01 17:57:16 +0530

    Added key file creation

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python3
 from cryptography.fernet import Fernet
+import os.path
 import io
 from flask import *
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def upload():
+    # with open('filekey.key', 'rb') as filekey:
+    #     key = filekey.read()
+    # return render_template("upload.html", key=key)
     return render_template("upload.html")
 
 
@@ -26,7 +30,13 @@ def encryptRequest():
     if request.method == 'POST':
         original_file = request.files['file']
         encrypted_data, encrypt_key = encrypt(
-            original_data=original_file.read())
+            original_data=original_file.read(),
+            )
+
+        print(encrypt_key) 
+        with open(f'{original_file.filename}.key', 'wb') as filekey:
+            filekey.write(encrypt_key)   
+
         return send_file(io.BytesIO(encrypted_data), as_attachment=True,
                          attachment_filename=original_file.filename,
                          mimetype=original_file.mimetype)
@@ -34,8 +44,10 @@ def encryptRequest():
 
 def encrypt(original_data):
     key = Fernet.generate_key()
+
     f = Fernet(key)
     encrypted_data = f.encrypt(original_data)
+    # print(key)
     return encrypted_data, key
 
 
